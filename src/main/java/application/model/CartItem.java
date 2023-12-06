150dfe23b49ad85928da12fa5159ae439ab1de8f
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-12-06 09:51:19 +0200

    added two more annotations

--- src/main/java/application/model/CartItem.java
+++ src/main/java/application/model/CartItem.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -23,10 +24,12 @@ public class CartItem {
     private Long id;
 
     @ManyToOne
+    @EqualsAndHashCode.Exclude
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
 
     @ManyToOne
+    @EqualsAndHashCode.Exclude
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
 
