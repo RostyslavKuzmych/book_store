c9b908deddae4369dbf72ec2bf6b99a8455603d1
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-12-23 18:28:56 +0200

    added logiс to add user role to registred user

--- src/main/java/application/service/impl/UserServiceImpl.java
+++ src/main/java/application/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import application.dto.user.UserRegistrationRequestDto;
 import application.dto.user.UserResponseDto;
 import application.exception.RegistrationException;
 import application.mapper.UserMapper;
+import application.model.Role;
 import application.model.User;
 import application.repository.UserRepository;
 import application.service.ShoppingCartService;
 import application.service.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private static final String EXCEPTION = "You are already registered!";
+    private static final Long USER_ROLE_ID = 1L;
     private final UserMapper userMapper;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -28,6 +31,7 @@ public class UserServiceImpl implements UserService {
         }
         User user = userMapper.toModel(requestDto);
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.setRoleSet(Set.of(new Role().setId(USER_ROLE_ID)));
         User savedUser = userRepository.save(user);
         shoppingCartService.createShoppingCart(savedUser);
         return userMapper.toDto(savedUser);
